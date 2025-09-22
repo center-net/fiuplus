f12de6086e51c9bf03a2c93bc30e5fa29ae21203
mrpya2007 <mrpya2007@gmail.com>
2025-09-22 21:26:38 +0300

    تجهيز القالب لمتعدد اللغات

--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -8,9 +8,11 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap" rel="stylesheet">
-{{-- @if (LaravelLocalization::setLocale() === 'ar') --}}
+@if (LaravelLocalization::setLocale() === 'ar')
     <link href="{{ asset('css/bootstrap.rtl.min.css') }}" rel="stylesheet">
-{{-- @endif --}}
+@else
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+@endif
 <!-- Styles -->
 <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
