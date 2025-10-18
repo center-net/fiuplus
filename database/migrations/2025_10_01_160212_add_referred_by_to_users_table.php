4e6e440f1453bc792595ee7291417479422b8e4d
center-net <36754485+center-net@users.noreply.github.com>
2025-10-18 14:30:38 +0300

    Update 2025_10_01_160212_add_referred_by_to_users_table.php

--- database/migrations/2025_10_01_160212_add_referred_by_to_users_table.php
+++ database/migrations/2025_10_01_160212_add_referred_by_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('referred_by')->nullable()->after('phone')->comment('اسم المستخدم الذي قام بالدعوة');
+            if (!Schema::hasColumn('users', 'referred_by')) {
+                $table->string('referred_by')->nullable()->after('phone')->comment('اسم المستخدم الذي قام بالدعوة');
+            }
         });
     }
 
